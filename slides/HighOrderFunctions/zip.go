69c0d99c92eaf68004d9977fc3ddc49d5ffec08c
Stefan Hans <uni@stefan-hans.de>
2017-06-02 23:03:19 +0200

    start trying article

---
+++ slides/HighOrderFunctions/zip.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListOfInt []int
+
+type listZipFunc func(x, y int) int
+
+func (list ListOfInt) Zip(otherList ListOfInt, f func(int, int) int) ListOfInt {
+	var out ListOfInt
+
+	for n, i := range list {
+		out = append(out, f(i, otherList[n]))
+	}
+	return out
+}
+
+func main() {
+
+	var list1 = ListOfInt{-20, -1, 0, 5, 2, 3}
+	var list2 = ListOfInt{-2, -10, 0, 2, 4, 3}
+
+	max := func(x, y int) int {
+		if x > y {
+			return x
+		}
+		return y
+	}
+
+	fmt.Printf("list%v: \nZip(%v, max) returns list%v\n",
+		list1, list2, list1.Zip(list2, max))
+
+}
