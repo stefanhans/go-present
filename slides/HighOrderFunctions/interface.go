dab206dde3b513560db1accd0e4c053f331ee8e2
Stefan Hans <uni@stefan-hans.de>
2017-07-29 10:46:48 +0200

    aggregate functions with monad and interface

--- slides/HighOrderFunctions/interface.go
+++ slides/HighOrderFunctions/interface.go
@@ -4,54 +4,49 @@ import (
 	"fmt"
 )
 
-// START OMIT
-type MonadType interface{}
+// START_IF OMIT
+type List []interface{}
 
-type ListForMonad []MonadType
+// END_IF OMIT
 
-type Aggregator interface {
-	GetNeutralElement() MonadType
-	GetAssocFunc() (func(...MonadType) MonadType)
-}
-
+// START_IMPL OMIT
 type Monad struct {
-	NeutralElement MonadType
-	AssocFunc      func(...MonadType) MonadType
+	NeutralElement interface{}
+	AssocFunc      func(...interface{}) interface{}
 }
 
-func (monad Monad) GetNeutralElement() MonadType {
-	return monad.NeutralElement
-}
+// END_IMPL OMIT
 
-func (monad Monad) GetAssocFunc() (func(...MonadType) MonadType) {
-	return monad.AssocFunc
-}
-
-func (list ListForMonad) Fold(monad Aggregator) MonadType {
-	out := monad.GetNeutralElement()
-	f := monad.GetAssocFunc()
+// START_FOLD OMIT
+func (list List) Fold(monad Monad) interface{} {
+	out := monad.NeutralElement
 	for _, i := range list {
-		out = f(out, i)
+		out = monad.AssocFunc(out, i)
 	}
 	return out
 }
-// END OMIT
 
+// END_FOLD OMIT
 
 func main() {
+	var listInt = List{-2, -1, 2, 3, 5}
+	monadInt := Monad{1, func(monad ...interface{}) interface{} {
+		return monad[0].(int) * monad[1].(int)
+	}}
+	fmt.Printf("List %v . Fold(listMonad) yields %v\n",
+		listInt, listInt.Fold(monadInt))
 
-	var listInt = ListForMonad{-2, -1, 2, 3, 5}
-	monadInt := Monad{1, func(monad ...MonadType) MonadType { return monad[0].(int) * monad[1].(int) }}
-	fmt.Printf("List %v: Fold(listMonad) yields %v\n", listInt, listInt.Fold(monadInt))
+	var listFloat = List{-2.5, -1.0, 2.0, 2.0, 3.0}
+	monadFloat := Monad{0.0, func(monad ...interface{}) interface{} {
+		return monad[0].(float64) + monad[1].(float64)
+	}}
+	fmt.Printf("List %v . Fold(monadFloat) yields %v\n",
+		listFloat, listFloat.Fold(monadFloat))
 
-
-	var listFloat = ListForMonad{-2.5, -1.0, 2.0, 2.0, 3.0}
-	monadFloat := Monad{0.0, func(monad ...MonadType) MonadType { return monad[0].(float64) + monad[1].(float64) }}
-	fmt.Printf("List %v: Fold(monadFloat) yields %v\n", listFloat, listFloat.Fold(monadFloat))
-
-	var c1, nc complex128 = -2.5+3i, 0+0i
-	var listComplex = ListForMonad{c1}
-	monadComplex := Monad{nc, func(monad ...MonadType) MonadType { return monad[0].(complex128) + monad[1].(complex128) }}
-	fmt.Printf("List %v: Fold(monadComplex) yields %v\n", listComplex, listComplex.Fold(monadComplex))
-
+	var listComplex = List{-2.5 + 3i, 1.5 + 2i}
+	monadComplex := Monad{0 + 0i, func(monad ...interface{}) interface{} {
+		return monad[0].(complex128) + monad[1].(complex128)
+	}}
+	fmt.Printf("List %v . Fold(monadComplex) yields %v\n",
+		listComplex, listComplex.Fold(monadComplex))
 }
---
+++ slides/HighOrderFunctions/interface0.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+
+// START OMIT
+type MonadType interface{}
+
+type ListForMonad []MonadType
+
+type Aggregator interface {
+	GetNeutralElement() MonadType
+	GetAssocFunc() (func(...MonadType) MonadType)
+}
+
+type Monad struct {
+	NeutralElement MonadType
+	AssocFunc      func(...MonadType) MonadType
+}
+
+func (monad Monad) GetNeutralElement() MonadType {
+	return monad.NeutralElement
+}
+
+func (monad Monad) GetAssocFunc() (func(...MonadType) MonadType) {
+	return monad.AssocFunc
+}
+
+func (list ListForMonad) Fold(monad Aggregator) MonadType {
+	out := monad.GetNeutralElement()
+	f := monad.GetAssocFunc()
+	for _, i := range list {
+		out = f(out, i)
+	}
+	return out
+}
+// END OMIT
+
+
+func main() {
+
+	var listInt = ListForMonad{-2, -1, 2, 3, 5}
+	monadInt := Monad{1, func(monad ...MonadType) MonadType { return monad[0].(int) * monad[1].(int) }}
+	fmt.Printf("List %v: Fold(listMonad) yields %v\n", listInt, listInt.Fold(monadInt))
+
+
+	var listFloat = ListForMonad{-2.5, -1.0, 2.0, 2.0, 3.0}
+	monadFloat := Monad{0.0, func(monad ...MonadType) MonadType { return monad[0].(float64) + monad[1].(float64) }}
+	fmt.Printf("List %v: Fold(monadFloat) yields %v\n", listFloat, listFloat.Fold(monadFloat))
+
+	var c1, nc complex128 = -2.5+3i, 0+0i
+	var listComplex = ListForMonad{c1}
+	monadComplex := Monad{nc, func(monad ...MonadType) MonadType { return monad[0].(complex128) + monad[1].(complex128) }}
+	fmt.Printf("List %v: Fold(monadComplex) yields %v\n", listComplex, listComplex.Fold(monadComplex))
+
+}
