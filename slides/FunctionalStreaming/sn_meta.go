dd91b56e85ea8cd384dbef2d25af917412c27cc9
Stefan Hans <uni@stefan-hans.de>
2017-11-06 20:50:30 +0100

    CmdExec PRINT/LOG

--- slides/FunctionalStreaming/sn_meta.go
+++ slides/FunctionalStreaming/sn_meta.go
@@ -1,80 +1,103 @@
 package main
 
 import (
-	"time"
 	"fmt"
+	"strings"
+	"time"
 )
 
 // START_1 OMIT
 type NodeOfInt struct {
-	in    chan int                                 // Input channel
-	cin   chan chan int                            // can be exchanged.
+	in  chan int      // Input channel
+	cin chan chan int // can be exchanged.
 
-	f     func(int) int                            // Function
-	cf    chan func(int) int                       // can be exchanged.
+	f  func(int) int      // Function
+	cf chan func(int) int // can be exchanged.
 
-	out   chan int                                 // Output channel
-	cout  chan chan int                            // can be exchanged.
+	out  chan int      // Output channel
+	cout chan chan int // can be exchanged.
 
-	metain chan string
+	name    string
+	metain  chan string
 	cmetain chan chan string
 
-	methods map[string]func() string
+	methods map[string]func(string) string
 
-	metaout chan string
+	metaout  chan string
 	cmetaout chan chan string
 
 	close chan bool // OMIT
 }
+
 // END_1 OMIT
 
 // START_2 OMIT
 func (node *NodeOfInt) Start() {
 	go func() {
-		for { select {
+		for {
+			select {
 
-		case in := <-node.in: node.out <- node.f(in) // Handle data (DEADLOCKS!) // HL
+			case in := <-node.in:
+				node.out <- node.f(in) // Handle data (DEADLOCKS!) // HL
 
-		case node.in = <-node.cin:   	            // Change input channel
-		case node.f = <-node.cf: 		            // Change function
-		case node.out = <-node.cout: 	            // Change output channel
+			case node.in = <-node.cin: // Change input channel
+			case node.f = <-node.cf: // Change function
+			case node.out = <-node.cout: // Change output channel
 
-		case methodName := <-node.metain:
-			if fmeta, ok := node.methods[methodName]; ok {
-				reply := fmeta()
-				fmt.Printf("method: %v: %v\n", methodName, reply)
-				node.metaout <-reply
-			} else {
-				fmt.Printf("Unknown method: %v\n", methodName)
+			case methodName := <-node.metain:
+				cmdAndString := strings.SplitAfterN(methodName, " ", 2)
+				/*
+					fmt.Printf("methodName: %v\n", methodName)
+					fmt.Printf("cmdAndString: %v\n", cmdAndString)
+					fmt.Printf("len(cmdAndString): %v\n", len(cmdAndString))
+					fmt.Printf("cmdAndString[0]: %v\n", cmdAndString[0])
+					fmt.Printf("cmdAndString[1]: %v\n", cmdAndString[1])
+				*/
+
+				if fmeta, ok := node.methods[strings.Trim(cmdAndString[0], " ")]; ok {
+					reply := fmeta(cmdAndString[1])
+					//fmt.Printf("cmdAndString[0]: %v\ncmdAndString[1]: %v\n", cmdAndString[0], cmdAndString[1])
+					node.metaout <- reply
+				} else {
+					fmt.Printf("UNKNOWN METHOD: %v%v\n", cmdAndString[0], cmdAndString[1])
+				}
+
+			case node.metain = <-node.cmetain: // Change metain channel
+			case node.metaout = <-node.cmetaout: // Change metaout channel
+
+			case str := <-node.metaout:
+				fmt.Printf("%v ", str)
+
+			case <-node.close:
+				return // OMIT
 			}
-
-		case node.metain = <-node.cmetain: 	            // Change metain channel
-		case node.metaout = <-node.cmetaout: 	            // Change metain channel
-
-		case str := <-node.metaout:
-			fmt.Printf("%v ", str)
-
-		case <-node.close: return // OMIT
 		}
-		}}()
+	}()
 }
+
 // END_2 OMIT
 
 // START_3 OMIT
-func NewNodeOfInt() *NodeOfInt {
+func NewNodeOfInt(name string) *NodeOfInt {
 	node := NodeOfInt{}
 	node.in = make(chan int)
 	node.cin = make(chan chan int)
-	node.f = func(in int) int { return in }        // Default function returns input value
+	node.f = func(in int) int { return in } // Default function returns input value
 	node.cf = make(chan func(int) int)
 	node.out = make(chan int)
 	node.cout = make(chan chan int)
 
+	node.name = name
 	node.metain = make(chan string)
 	node.cmetain = make(chan chan string)
 
-	node.methods = make(map[string]func() string)
-	node.methods["print"] = func() string { return "123" }
+	node.methods = make(map[string]func(string) string)
+	node.methods["print"] = func(str string) string {
+		return fmt.Sprintf("print %v\nPRINT: (%T) \"%v\"", str, node, node.name)
+	}
+	node.methods["log"] = func(str string) string {
+		return fmt.Sprintf("log %v\nLOG: (%T) \"%v\"", str, node, node.name)
+	}
 
 	node.metaout = make(chan string)
 	node.cmetaout = make(chan chan string)
@@ -83,6 +106,7 @@ func NewNodeOfInt() *NodeOfInt {
 	node.Start()
 	return &node
 }
+
 // END_3 OMIT
 
 // START_5 OMIT
@@ -91,46 +115,80 @@ func (node *NodeOfInt) Connect(nextNode *NodeOfInt) *NodeOfInt {
 	node.cmetaout <- nextNode.metain
 	return nextNode
 }
+
 // END_5 OMIT
 
 // START_NODE_SETFUNC OMIT
 func (node *NodeOfInt) SetFunc(f func(int) int) { node.cf <- f }
+
 // END_NODE_SETFUNC OMIT
 
 // START_PRINTF OMIT
 func (node *NodeOfInt) Printf(format string) {
-	go func() { for { select {
-	case in := <-node.out: fmt.Printf(format, in)		// HL
-	}}}()
+	go func() {
+		for {
+			select {
+			case in := <-node.out:
+				fmt.Printf(format, in) // HL
+			}
+		}
+	}()
 }
+
 // END_PRINTF OMIT
 
+// START_PRINTF OMIT
+func (node *NodeOfInt) MetaPrintf(format string) {
+	go func() {
+		for {
+			select {
+			case metaout := <-node.metaout:
+				fmt.Printf(format, strings.SplitAfterN(metaout, "\n", 2)[1]) // HL
+			}
+		}
+	}()
+}
+
+// END_PRINTF OMIT
 
 // START_ProduceAtMs OMIT
 func (node *NodeOfInt) ProduceAtMs(n time.Duration) *NodeOfInt {
-	go func() { for { select {
-	default: node.in <- 0 }	               // Trigger permanently // HL
-		time.Sleep(time.Millisecond * n)	      // with delay in ms // HL
-	}}()
+	go func() {
+		for {
+			select {
+			default:
+				node.in <- 0
+			} // Trigger permanently // HL
+			time.Sleep(time.Millisecond * n) // with delay in ms // HL
+		}
+	}()
 	return node
 }
+
 // END_ProduceAtMs OMIT
 
+// START_NODE_SETFUNC OMIT
+func (node *NodeOfInt) ExecCmd(cmd string) { node.metain <- cmd + " " }
+
+// END_NODE_SETFUNC OMIT
 
 func main() {
-	node_1, node_2 := NewNodeOfInt(), NewNodeOfInt()            // nodes' creation // HL
-	var i int                                                   //
-	node_1.SetFunc(func(in int) int { i++; return in + i })     //
-	node_2.SetFunc(func(in int) int { return in * 2 })          //
+	node_1, node_2, node_3 := NewNodeOfInt("node_1"), NewNodeOfInt("node_2"), NewNodeOfInt("node_3") // nodes' creation // HL
+	var i int                                                                                        //
+	node_1.SetFunc(func(in int) int { i++; return in + i })                                          //
+	node_2.SetFunc(func(in int) int { return in * 2 })                                               //
 
-	node_2.metain <- "print"
+	node_1.Connect(node_2).Connect(node_3).Printf("%v ") // stream configuration // HL
+	node_1.ProduceAtMs(50)                               // sending data  // HL
 
+	node_3.MetaPrintf("\n%v\n")
 
-	node_1.Connect(node_2).Printf("%v ")                        // stream configuration // HL
-	node_1.ProduceAtMs(50)                                      // sending data  // HL
+	node_1.ExecCmd("print")
+	node_2.ExecCmd("log")
+
 	time.Sleep(time.Second)
 	fmt.Println()
 
-	node_2.SetFunc(func(in int) int { return in * 10 })         // change function // HL
+	node_2.SetFunc(func(in int) int { return in * 10 }) // change function // HL
 	time.Sleep(time.Second)
 }
