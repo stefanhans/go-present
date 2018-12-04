d565bf6d2ad775893b02c26afcbe455441d6e55c
“stefanhans” <uni@stefan-hans.de>
2018-12-04 11:33:00 +0100

    Core version before playing with GCP

--- slides/CloudFunctions/helloworld_1.bash
+++ slides/CloudFunctions/helloworld_1.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd functions
+
 GCP_PROJECT="gke-serverless-211907"
 GOOGLE_APPLICATION_CREDENTIALS="/Users/stefan/.secret/gke-serverless-211907-181ed186fa7f.json"
 
-gcloud alpha functions call --region=europe-west1 HelloWorld
+gcloud alpha functions deploy HelloWorld --region europe-west1 --runtime go111 --trigger-http
--- slides/CloudFunctions/helloworld_3.bash
+++ slides/CloudFunctions/helloworld_3.bash
@@ -5,6 +5,4 @@ cd functions
 GCP_PROJECT="gke-serverless-211907"
 GOOGLE_APPLICATION_CREDENTIALS="/Users/stefan/.secret/gke-serverless-211907-181ed186fa7f.json"
 
-gcloud alpha functions deploy HelloWorld --region europe-west1 --memory 128MB --runtime go111 --trigger-http
-
-#gcloud alpha functions call --region=europe-west1 HelloWorld
+gcloud alpha functions call --region=europe-west1 HelloWorld
